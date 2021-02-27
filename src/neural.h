58d68244d18e3bedc35720fa96f82072c33b590a
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-02-27 00:16:55 -0600

    some basic things
    idk why this have an error in the line 16 :weary:

--- src/neural.h
+++ src/neural.h
@@ -7,11 +7,20 @@ class node
 public:
     float added_weight = 1,
           weigth = 1,
-          input = 0;
+          input_node = 0;
+    void adding(vector<node> node_vector)
+    {
+
+        for (int i = 0; i < node_vector.size(); i++)
+        {
+            input_node += (node_vector.at(i).weigth * node_vector.at(i).input_node) * node_vector.at(i).added_weight;
+        }
+    }
 };
 // this is the layers of the neural network
 struct layer
 {
+
     int id;
     bool last;
     vector<node> nodes;
@@ -23,6 +32,7 @@ struct layer
         for (size; size > 0; *size--)
             nodes.push_back(n);
     };
+
     //this change the weight of the node
     void change_weight_node(int id, float new_weight)
     {
@@ -41,12 +51,9 @@ struct layer
         // this is for add the nodes to the new layer
         int *value = &size_nodes_layer->at(0);
         new_layer.add_nodes(value);
-
         // this erase the first value of size:nodes_layer
-        size_nodes_layer->erase(size_nodes_layer->begin());
-        // this reduce the code
-        *number_of_layers--;
-        //if x>0 do this else do this
+        size_nodes_layer->erase(size_nodes_layer->begin()); // this reduce the code
+        *number_of_layers--;                                //if x>0 do this else do this
         if (*number_of_layers)
         {
             // basic concurrency
