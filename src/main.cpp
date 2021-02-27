99332e1698c5f6ed88851f63bb1ea1e18f5808c5
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-02-27 16:04:33 -0600

    idk why this doesnt work

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-
+#include "neural.h"
 using namespace std;
 
 int main(){
-    return 0;
+    vector<int> v;
+    for (int i = 0; i < 14; i++)
+        v.push_back(i);
+
+    /**
+     * no instance of constructor "std::__1::vector<_Tp, _Allocator>::vector [with _Tp=int, _Allocator=std::__1::allocator<int>]" matches the argument list -- argument types are: (int, int, int, int, int)
+    */
+    layer x;
+    x.add_new_layer(&v);
+    layer x2 = x.get_last();
+    for (int i = 0; i < x2.nodes.size(); i++)
+    {
+        cout << x2.nodes.at(i).input_node << endl;
+    }
+        return 0;
 }
--- src/neural.h
+++ src/neural.h
@@ -5,15 +5,15 @@ using namespace std;
 class node
 {
 public:
-    float added_weight = 1,
-          weigth = 1,
-          input_node = 0;
+    float added_weight_node = 1;
+    float weigth = 1;
+    float input_node = 0;
     void adding(vector<node> node_vector)
     {
 
         for (int i = 0; i < node_vector.size(); i++)
         {
-            input_node += (node_vector.at(i).weigth * node_vector.at(i).input_node) * node_vector.at(i).added_weight;
+            input_node += (node_vector.at(i).weigth * node_vector.at(i).input_node) * node_vector.at(i).added_weight_node;
         }
     }
 };
@@ -29,47 +29,82 @@ struct layer
     void add_nodes(int *size)
     {
         node n;
-        for (size; size > 0; *size--)
+        for (*size; *size > 0; *size--)
             nodes.push_back(n);
     };
 
     //this change the weight of the node
-    void change_weight_node(int id, float new_weight)
+    void change_weight_nodes()
     {
-        nodes.at(id).weigth = new_weight;
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            nodes.at(i).weigth += -1 + (rand() % 140) * 0.0223;
+            nodes.at(i).added_weight_node += -1 + (rand() % 140) * 0.0123;
+        }
     }
     // this is for add a new layer
-    void add_new_layer(int *number_of_layers, vector<int> *size_nodes_layer)
+    void add_new_layer(vector<int> *size_nodes_layer)
     {
 
         // this is the new layer
         layer new_layer = {
-            id + 1,             // 1->2->3->4->5
-            !*number_of_layers, //if x>0=true else = false
+            id + 1,                    // 1->2->3->4->5
+            !size_nodes_layer->size(), //if x>0=true else = false
         };
 
         // this is for add the nodes to the new layer
         int *value = &size_nodes_layer->at(0);
         new_layer.add_nodes(value);
+        // this change the weight and the added weight for generate a new layer with new  values
+        new_layer.change_weight_nodes();
         // this erase the first value of size:nodes_layer
         size_nodes_layer->erase(size_nodes_layer->begin()); // this reduce the code
-        *number_of_layers--;                                //if x>0 do this else do this
-        if (*number_of_layers)
+                                                            //if x>0 do this else do this
+        if (size_nodes_layer->size())
         {
             // basic concurrency
-            new_layer.add_new_layer(number_of_layers, size_nodes_layer);
+            new_layer.add_new_layer(size_nodes_layer);
             next_layer = &new_layer;
         }
     }
-    void change_node(){}
-    void summation() {
-
-
+    // this is for get the last value
+    layer get_last()
+    {
+        
+            return next_layer->get_last();
+        
     }
-};
-class neural_network
-{
-    vector<vector<node> > layers;
+    // this made the summation and the send you the output
+    void summation()
+    {
+        if (!last)
+        {
+            //wx1*wx2*wx3*wx4*wx5
+            /**         xw \
+             *    / xw<-xw<-xw
+             * xw<- xw<-xw<-xw->xw
+             *    \ xw<-xw<-xw 
+             *          xw /
+            */
+            for (int i = 0; i < next_layer->nodes.size(); i++)
+            {
+                next_layer->nodes.at(i).adding(nodes);
+            }
+            next_layer->summation();
+        }
+    }
+    void visualize()
+    {
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            cout << nodes.at(i).input_node << " " << (nodes.at(i).weigth);
+        }
+        if (next_layer != NULL)
+        {
+            cout << endl;
+            next_layer->visualize();
+        }
+    }
 };
 /**
  * 
