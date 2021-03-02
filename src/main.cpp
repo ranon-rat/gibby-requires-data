b239947c098f5c0dc6364b0d52c07fe457e30e7f
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-03-01 23:16:23 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,15 @@ int main(){
     /**
      * no instance of constructor "std::__1::vector<_Tp, _Allocator>::vector [with _Tp=int, _Allocator=std::__1::allocator<int>]" matches the argument list -- argument types are: (int, int, int, int, int)
     */
+    cout << "============STARTING=============" << endl;
     layer x;
     x.generate_neural_network(1, v, 1);
     x.visualize();
 
-    cout << "=========================" << endl;
+    cout << "=============INPUTS============" << endl;
     x.next_layer->input_nodes(v2);
     x.visualize();
-    cout << "=========================" << endl;
+    cout << "==============SUMMATION===========" << endl;
     x.summation();
     x.visualize();
 
