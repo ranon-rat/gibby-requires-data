b5acec147a9ccd4f26881719679a230e63966f0b
ranon-rat <temporalsantiagoloco@gmail.com>
2021-03-16 02:32:59 -0600

    new things

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     vector<int> v = {2, 2, 2};
-    vector<double> v2 = {1234};
+    vector<double> v2 = {0.1};
 
     /**
      * no instance of constructor "std::__1::vector<_Tp, _Allocator>::vector [with _Tp=int, _Allocator=std::__1::allocator<int>]" matches the argument list -- argument types are: (int, int, int, int, int)
@@ -19,8 +19,9 @@ int main(){
     x.next_layer->input_nodes(v2);
     x.visualize();
     cout << "==============SUMMATION===========" << endl;
-    x.summation();
-    x.visualize();
+    x.next_layer->summation();
+    layer *i = x.get_last();
+    cout << i->nodes.at(0).input_node << endl;
 
     //x.visualize();
 
--- src/neural.cpp
+++ src/neural.cpp
@@ -1,7 +1,8 @@
 #include "neural.h"
 #include <iostream>
 #include <stdexcept>
-void node::adding(const std::vector<node>& nodes) {
+void node::adding(const std::vector<node> &nodes)
+{
     for (const auto &n : nodes)
     {
         input_node += (n.weight * n.input_node) / added_weight_node;
@@ -19,28 +20,34 @@ layer *layer::get_last()
 void layer::input_nodes(std::vector<double> n)
 {
     if (n.size() != nodes.size())
-        throw std::invalid_argument("the size is different");
+        throw std::invalid_argument("the size is different" + std::to_string(nodes.size()));
     for (int i = 0; i < n.size(); i++)
         nodes.at(i).input_node = n.at(i);
 }
 
-void layer::add_nodes(int amount) {
+void layer::add_nodes(int amount)
+{
     node n;
-    for (int i = 0; i < amount; i++) nodes.push_back(n);
+    for (int i = 0; i < amount; i++)
+        nodes.push_back(n);
 }
 
-void layer::randomize_weights() {
-    for (auto& node : nodes) {
-        node.weight  += -1 + (std::rand() % 140) * 0.0223;
-        node.added_weight_node  += -1 + (std::rand() % 140) * 0.0123;
+void layer::randomize_weights()
+{
+    for (auto &node : nodes)
+    {
+        node.weight += -1 + (std::rand() % 140) * 0.0223;
+        node.added_weight_node += -1 + (std::rand() % 140) * 0.0123;
     }
 }
 
-void layer::add_layers(const std::vector<int>& layer_sizes) {
-    layer* p_last = get_last();
+void layer::add_layers(const std::vector<int> &layer_sizes)
+{
+    layer *p_last = get_last();
     // note that layer_sizes.size() must not exceed INT_MAX
-    for (int i = 0; i < static_cast<int>(layer_sizes.size()); i++) {                        
-        layer* p_new_layer = new layer;
+    for (int i = 0; i < static_cast<int>(layer_sizes.size()); i++)
+    {
+        layer *p_new_layer = new layer;
         p_new_layer->id = p_last->id + 1;
         p_new_layer->add_nodes(layer_sizes[i]);
         p_new_layer->randomize_weights();
@@ -55,25 +62,29 @@ void layer::update_all_layers()
         return;
     next_layer->update_all_layers();
 }
+
 void layer::summation() {
     if (!next_layer) return;
     for (auto& node : next_layer->nodes) {
         node.adding(this->nodes);
     }
+    next_layer->summation();
 }
 void layer::generate_neural_network(int inputs_size, std::vector<int> hidden_layers, int outputs_size)
 {
-		id = 0;
+    id = 0;
     std::vector<int> v = {inputs_size};
     v.insert(std::end(v), std::begin(hidden_layers), std::end(hidden_layers));
     v.push_back(outputs_size);
     add_layers(v);
 }
-void layer::visualize() {
+void layer::visualize()
+{
     std::cout << "The id is " << id << std::endl;
     for (auto i : nodes)
         std::cout << "the weight is " << i.weight << " \nthe input is " << i.input_node << std::endl;
-    if (!next_layer) return;
+    if (!next_layer)
+        return;
     std::cout << "=================================================\n";
     next_layer->visualize();
 }
